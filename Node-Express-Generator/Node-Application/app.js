46527e43e65ff825e36a5e3e84d7d7e391743497
Zain Zulfiqar <zainzulfiqarmaknojia@gmail.com>
2020-08-19 12:57:35 +0500

    Completed with Express Sessions

--- Node-Express-Generator/Node-Application/app.js
+++ Node-Express-Generator/Node-Application/app.js
@@ -35,11 +35,44 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+//app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    return next(err);
+  } else {
+    if (req.session.user === "authenticated") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
